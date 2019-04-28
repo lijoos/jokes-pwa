1d652503ec97c04fa109f723ec840b7cbbe0da85
Lilo Joseph <lilojoseph@Lilos-MacBook-Pro.local>
2019-04-28 19:56:00 +0700

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'jokes';
+ // update = false;
+  jokes: any;
+  constructor(swUpdate: SwUpdate, private data: DataService) {
+    swUpdate.available.subscribe(event => {
+       // this.update = true;
+       swUpdate.activateUpdate().then(() => {
+       document.location.reload();
+       });
+    });
+  }
+  ngOnInit(): void {
+    this.data.getJokes().subscribe(res => {
+     this.jokes = res;
+    });
+  }
 }
